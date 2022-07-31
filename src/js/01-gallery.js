c29637fbd356891f8c496ac7914322cd8cd7105e
Viktor-117 <viktor.hushchin@gmail.com>
2022-07-31 20:02:15 +0300

    first update

---
+++ src/js/01-gallery.js
@@ -0,0 +1,6 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+import { galleryTemplate } from "./gallery-template.js";
+
+console.log(galleryItems);
+console.log(galleryTemplate);
